345e834cc2d146de9770abcdab2b8c314aeaa30a
fay2wk <ngsiwai@gmail.com>
2016-06-30 22:02:10 +0800

    Update with admin model, g chart

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :wikis
+  devise_for :admins
+
+  resources :wikis do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

0789c51e0ec9428b0719f33e2ba8b05a1b8382fe
fay2wk <ngsiwai@gmail.com>
2016-07-01 02:41:30 +0800

    Update routes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,13 +5,12 @@ class StaticController < ApplicationController
 
   def dash
     @tracks = current_user.tracks.limit(5).order("created_at DESC")
-
     @bar_chart = Gchart.bar(:data => Track.where(:user_id == current_user).pluck(:hours))
     @line_chart = Gchart.line(:data => Track.where(:user_id == current_user).pluck(:mood))
     @line_chart2 = Gchart.line(:data => Track.where(:user_id == current_user).pluck(:productivity))
   end
 
-  def dash
+  def admin
     @tracks = Track.all.order("created_at DESC")
     @users = User.all.order("created_at DESC")
     @wikis = Wiki.all.order("created_at DESC")
